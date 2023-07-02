657f9b4e94b8cf49dee653dccfa73e19e00a8fe3
666seasir666 <2429016980@qq.com>
2023-07-02 23:03:34 +0800

    hou

--- js/Order.js
+++ js/Order.js
@@ -1,11 +1,11 @@
 var arr = ['https://yanxuan-item.nosdn.127.net/7170d6147878a099914b7bb7ce21773f.png', '	https://yanxuan-item.nosdn.127.net/6d8d6772df51fa60c2e899a16ac34718.png', 'https://yanxuan-item.nosdn.127.net/ea1ec719c9a860eedb0cb7ef31ada4af.png', 'https://yanxuan-item.nosdn.127.net/c3b4e3640f77bb72ec4d043ef2e2a0cd.png']
 
-var img = document.querySelector('.carousel-item .slider img')
-
+var img = document.querySelectorAll('.slider a')
+console.log(img);
 
 var index = 0;
 setInterval(function () {
-    index = ++index === arr[lengthE] ?  0 : index;
+    index = ++index === arr.length ?  0 : index;
         img.src = arr[index];
-    console.log(img);
+    
 }, 1000)
