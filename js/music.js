5131b4ecf4a9728c23cf4bd5320e098ac226e46b
666seasir666 <2429016980@qq.com>
2023-08-26 14:20:13 +0800

    music

--- js/music.js
+++ js/music.js
@@ -4,6 +4,6 @@ const core = window._PlayerCore
 core.AppendSongOnTail({
     name: '正在播放 七里香',
     id: 2,
-    src: 'https://freetyst.nf.migu.cn/public/product9th/product45/2022/07/2210/2009%E5%B9%B406%E6%9C%8826%E6%97%A5%E5%8D%9A%E5%B0%94%E6%99%AE%E6%96%AF/%E6%A0%87%E6%B8%85%E9%AB%98%E6%B8%85/MP3_320_16_Stero/60054701934104032.mp3',
+    src: './music/七里香 - 周杰伦.mp3',
     img: 'https://i.postimg.cc/d0Z8RcPh/1692978507264.jpg'   //歌曲头像
 })
